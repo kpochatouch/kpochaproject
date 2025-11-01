441b5f0336d9ea10c535ba6bb92d6605b18eebda
kpochatouch <kpochaout@gmail.com>
2025-11-01 16:29:24 +0100

    Blocked camera fixed for awsLiveness.js

--- apps/web/src/lib/aws-exports.js
+++ apps/web/src/lib/aws-exports.js
@@ -1,6 +1,6 @@
-// e.g. apps/web/src/lib/aws-exports.js
+// apps/web/src/lib/aws-exports.js
 export default {
   aws_project_region: import.meta.env.VITE_AWS_REGION,
-  aws_cognito_region: import.meta.env.VITE_AWS_REGION,
   aws_cognito_identity_pool_id: import.meta.env.VITE_AWS_COGNITO_IDENTITY_POOL_ID,
+  aws_cognito_region: import.meta.env.VITE_AWS_REGION,
 };
