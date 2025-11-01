c39f0e1a9ada61789565bab8fc57772d39373923
kpochatouch <kpochaout@gmail.com>
2025-11-01 10:33:01 +0100

    Final updated awsLiveness.js

--- apps/web/src/aws-exports.js
+++
@@ -1,6 +0,0 @@
-// Minimal Amplify config for Rekognition Face Liveness
-export default {
-  aws_project_region: import.meta.env.VITE_AWS_REGION || "us-east-1",
-  aws_cognito_identity_pool_id: import.meta.env.VITE_AWS_COGNITO_IDENTITY_POOL_ID,
-  aws_cognito_region: import.meta.env.VITE_AWS_REGION || "us-east-1",
-};
